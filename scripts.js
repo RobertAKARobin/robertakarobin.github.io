ce2387f90f132b4ec47ed6479f6ccc3283c419e0
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-13 11:26:26 -0500

    Mode toggling works

--- scripts.js
+++ scripts.js
@@ -11,6 +11,34 @@ window.onload = function(){
     h.el("#street").innerHTML = response["street"] + "<br />";
   });
 
+  (function cssModes(){
+    var mode, modes = {
+      "business": {},
+      "party":    {"media": "base"},
+      "print":    {"theme": "business", "media": "print"}
+    };
+    var els = {
+      list: h.el("#modes"),
+      theme: h.el("#theme"),
+      media: h.el("#media")
+    }
+    for(mode in modes){
+      createModeOption(mode, modes[mode]);
+    }
+    function createModeOption(name, info){
+      var link = h.setEl("A");
+      link.textContent = name.charAt(0).toUpperCase() + name.substring(1) + " Mode";
+      link.setAttribute("data-theme", (info["theme"] || name || "business"));
+      link.setAttribute("data-media", (info["media"] || "screen"));
+      link.addEventListener("click", optionListener);
+      els.list.appendChild(link);
+    }
+    function optionListener(){
+      els.theme.href = "css/" + this.getAttribute("data-theme") + ".css";
+      els.media.href = "css/" + this.getAttribute("data-media") + ".css";
+    }
+  }());
+
   h.ajax("GET", "php/count.php", function loadCount(response){
     h.el("#promptNum").textContent = response;
   });
@@ -33,6 +61,9 @@ window.onload = function(){
         callback(this.responseText);
       }
       http.send();
+    }
+    out.setEl = function(type){
+      return document.createElement(type);
     }
     return out;
   }
