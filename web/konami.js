088460bd1c498d67f80b854378b833c2d9e5238c
RobertAKARobin <robertgfthomas@gmail.com>
2018-11-18 13:58:50 -0600

    Wrap all JS in IIFEs

--- web/konami.js
+++ web/konami.js
@@ -1,21 +1,24 @@
-const konami = {
-	event: new Event('konami'),
-	input: [],
-	sequence: [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-}
-
-function konamiListener(event){
-	konami.input.push(event.keyCode)
-	let inputString = konami.input.join('.'),
-		sequenceString = konami.sequence.join('.')
-	if(sequenceString.indexOf(inputString) < 0){
-		konami.input = []
-	}else if(sequenceString == inputString){
-		konami.input = []
-		window.dispatchEvent(konami.event)
+(function(){
+	'use strict'
+	const konami = {
+		event: new Event('konami'),
+		input: [],
+		sequence: [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 	}
-}
 
-document.addEventListener('DOMContentLoaded', ()=>{
-	window.addEventListener('keyup', konamiListener)
-})
+	function konamiListener(event){
+		konami.input.push(event.keyCode)
+		let inputString = konami.input.join('.'),
+			sequenceString = konami.sequence.join('.')
+		if(sequenceString.indexOf(inputString) < 0){
+			konami.input = []
+		}else if(sequenceString == inputString){
+			konami.input = []
+			window.dispatchEvent(konami.event)
+		}
+	}
+
+	document.addEventListener('DOMContentLoaded', ()=>{
+		window.addEventListener('keyup', konamiListener)
+	})
+})();
--- web/templates.js
+++ web/templates.js
@@ -1,40 +1,43 @@
-const template = {
-	triggerElements: [],
-	viewName: '',
-}
+(function(){
+	'use strict'
+	const template = {
+		triggerElements: [],
+		viewName: '',
+	}
 
-document.addEventListener('DOMContentLoaded', ()=>{
-	template.triggerElements = Array.from(document.querySelectorAll('[data-template-triggers] a'))
-	template.triggerElements.forEach((element)=>{
-		element.addEventListener('click', onTemplateTrigger)
+	document.addEventListener('DOMContentLoaded', ()=>{
+		template.triggerElements = Array.from(document.querySelectorAll('[data-template-triggers] a'))
+		template.triggerElements.forEach((element)=>{
+			element.addEventListener('click', onTemplateTrigger)
+		})
+
+		window.addEventListener('hashchange', onHashChange)
+		onHashChange()
 	})
 
-	window.addEventListener('hashchange', onHashChange)
-	onHashChange()
-})
 
 
-
-function onTemplateTrigger(event){
-	let element = this
-	location.hash = template.viewName = element.textContent.toLowerCase()
-}
-
-function onHashChange(event){
-	const allowedViewNames = ['one-pager','extended','geocities']
-	template.viewName = (location.hash || '').substring(1)
-	if(!allowedViewNames.includes(template.viewName)){
-		template.viewName = allowedViewNames[0]
+	function onTemplateTrigger(event){
+		let element = this
+		location.hash = template.viewName = element.textContent.toLowerCase()
 	}
-	document.querySelector('body').className = template.viewName
-	template.triggerElements.forEach(highlightActiveTemplate)
-}
 
-function highlightActiveTemplate(element){
-	let elementTemplateName = element.textContent.toLowerCase()
-	if(elementTemplateName == template.viewName){
-		element.setAttribute('data-template-active', 'true')
-	}else{
-		element.removeAttribute('data-template-active')
+	function onHashChange(event){
+		const allowedViewNames = ['one-pager','extended','geocities']
+		template.viewName = (location.hash || '').substring(1)
+		if(!allowedViewNames.includes(template.viewName)){
+			template.viewName = allowedViewNames[0]
+		}
+		document.querySelector('body').className = template.viewName
+		template.triggerElements.forEach(highlightActiveTemplate)
 	}
-}
+
+	function highlightActiveTemplate(element){
+		let elementTemplateName = element.textContent.toLowerCase()
+		if(elementTemplateName == template.viewName){
+			element.setAttribute('data-template-active', 'true')
+		}else{
+			element.removeAttribute('data-template-active')
+		}
+	}
+})();
